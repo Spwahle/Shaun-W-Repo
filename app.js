6cdaa6a24e809ce4cf05b30432558b03277c3cb1
Shaun Wahle <shaun.wahle@gmail.com>
2017-05-13 09:21:29 -0700

    Fixed linter issues

--- app.js
+++ app.js
@@ -7,76 +7,76 @@ var userAnswerTotal = 0;
 
 function Question1() {
     //Q1 is a Y/N that utilizes if else
-    var answer3 = prompt('Do I enjoy running?').toLowerCase();
-    console.log('The user answered ' + answer3 + ' to question 3.');
-    if (answer3 === 'yes' || answer3 === 'y') {
-        alert('I mean.....I mostly like running,');
-    } else if (answer3 === 'no' || answer3 === 'n') {
-        alert('You\'re not quite correct. I mostly enjoy running');
-        userAnswerTotal++;
-    } else {
-        alert('Answer with either yes or no / y or n');
-    }
+  var answer3 = prompt('Do I enjoy running?').toLowerCase();
+  console.log('The user answered ' + answer3 + ' to question 3.');
+  if (answer3 === 'yes' || answer3 === 'y') {
+    alert('I mean.....I mostly like running,');
+  } else if (answer3 === 'no' || answer3 === 'n') {
+    alert('You\'re not quite correct. I mostly enjoy running');
+    userAnswerTotal++;
+  } else {
+    alert('Answer with either yes or no / y or n');
+  }
 }
 
 function Question2() {
     //Q2 is a Y/N that utilizes if else
-    var answer2 = prompt('Do you think I have any pets?').toLowerCase();
+  var answer2 = prompt('Do you think I have any pets?').toLowerCase();
 
-    console.log('The user answered ' + answer2 + ' to question 2.');
+  console.log('The user answered ' + answer2 + ' to question 2.');
 
-    if (answer2 === 'yes' || answer2 === 'y') {
-        alert('Yes! I have one cat');
-        userAnswerTotal++;
-    } else if (answer2 === 'no' || answer2 === 'n') {
-        alert('Nope, I actually have one cat');
-    } else {
-        alert('Answer with either yes or no / y or n');
-    }
+  if (answer2 === 'yes' || answer2 === 'y') {
+    alert('Yes! I have one cat');
+    userAnswerTotal++;
+  } else if (answer2 === 'no' || answer2 === 'n') {
+    alert('Nope, I actually have one cat');
+  } else {
+    alert('Answer with either yes or no / y or n');
+  }
 }
 
 function Question3() {
     //Q3 is another if else
-    var answer4 = prompt('Do I like to coding?').toLowerCase();
-    console.log('The user answered ' + answer4 + ' to question 4.');
+  var answer4 = prompt('Do I like to coding?').toLowerCase();
+  console.log('The user answered ' + answer4 + ' to question 4.');
 
-    if (answer4 === 'yes' || answer4 === 'y') {
-        alert('Yep, I really enjoy coding so far!');
-        userAnswerTotal++;
-    } else if (answer4 === 'no' || answer4 === 'n') {
-        alert('Sorry you\'re wrong, I love coding');
-    } else {
-        alert('Answer with either yes or no / y or n');
-    }
+  if (answer4 === 'yes' || answer4 === 'y') {
+    alert('Yep, I really enjoy coding so far!');
+    userAnswerTotal++;
+  } else if (answer4 === 'no' || answer4 === 'n') {
+    alert('Sorry you\'re wrong, I love coding');
+  } else {
+    alert('Answer with either yes or no / y or n');
+  }
 }
 
 function Question4() {
     //Q4 is the another Y/N with if else
-    var answer5 = prompt('Did I work 10 years in Food Technology?').toLowerCase();
-    console.log('The user answered ' + answer5 + ' to question 5.');
-    if (answer5 === 'yes' || answer5 === 'y') {
-        alert('No! How old do you think I am?!');
-    } else if (answer5 === 'no' || answer5 === 'n') {
-        alert('Yep! I actually worked 5 years in food science making junk food.');
-        userAnswerTotal++;
-    } else {
-        alert('Answer with either yes or no / y or n');
-    }
+  var answer5 = prompt('Did I work 10 years in Food Technology?').toLowerCase();
+  console.log('The user answered ' + answer5 + ' to question 5.');
+  if (answer5 === 'yes' || answer5 === 'y') {
+    alert('No! How old do you think I am?!');
+  } else if (answer5 === 'no' || answer5 === 'n') {
+    alert('Yep! I actually worked 5 years in food science making junk food.');
+    userAnswerTotal++;
+  } else {
+    alert('Answer with either yes or no / y or n');
+  }
 }
 
 function Question5() {
     //Q5 is a Y/N that uses a if else statement
-    var answer1 = prompt('Do you think I like to travel and cook?').toLowerCase();
-    console.log('The user answered ' + answer1 + ' to question 1.');
+  var answer1 = prompt('Do you think I like to travel and cook?').toLowerCase();
+  console.log('The user answered ' + answer1 + ' to question 1.');
 
-    if (answer1 === 'no' || answer1 === 'n') {
-        alert('Sorry that\'s wrong, I LOVE to travel and cook.');
-    } else if (answer1 === 'yes' || answer1 === 'y') {
-        alert('YES! I actually love traveling and cooking.');
-        userAnswerTotal++;
-    } else {
-        alert('Answer with either yes or no / y or n');
-    }
+  if (answer1 === 'no' || answer1 === 'n') {
+    alert('Sorry that\'s wrong, I LOVE to travel and cook.');
+  } else if (answer1 === 'yes' || answer1 === 'y') {
+    alert('YES! I actually love traveling and cooking.');
+    userAnswerTotal++;
+  } else {
+    alert('Answer with either yes or no / y or n');
+  }
 }
 
 // function Question6() {
@@ -106,53 +106,53 @@ function Question5() {
 //     }
 function Question6() {
     // Asks the user to guess a number between 1 and 100
-    var myAge = 29; // Sets the number to guess.
-    var q6tries = 0; // Keeps track of the number of attempted answers.
+  var myAge = 29; // Sets the number to guess.
+  var q6tries = 0; // Keeps track of the number of attempted answers.
 
-    while (q6tries < 4) {
-        var answer6 = parseInt(prompt('Guess how old I am (You have ' + (4 - q6tries) + ' tries remaining.)')); // How many tries they have left.
-        if (answer6 === myAge) {
-            alert('You guessed it! I am ' + myAge + '!');
-            q6tries = 4;
-            userAnswerTotal++;
-        } else if (answer6 < myAge) {
-            alert('Too low! I am older than that!');
-        } else if (answer6 > myAge) {
-            alert('Too high, I am younger than that!');
-        } else {
-            alert('Try again--enter a number between 1 and 100 this time!');
-        }
-        q6tries++;
+  while (q6tries < 4) {
+    var answer6 = parseInt(prompt('Guess how old I am (You have ' + (4 - q6tries) + ' tries remaining.)')); // How many tries they have left.
+    if (answer6 === myAge) {
+      alert('You guessed it! I am ' + myAge + '!');
+      q6tries = 4;
+      userAnswerTotal++;
+    } else if (answer6 < myAge) {
+      alert('Too low! I am older than that!');
+    } else if (answer6 > myAge) {
+      alert('Too high, I am younger than that!');
+    } else {
+      alert('Try again--enter a number between 1 and 100 this time!');
     }
+    q6tries++;
+  }
 }
 
 function Question7() {
     //Q7 is a mess of a question but it uses a loop inside of a loop. It has an array that will print if the user succeeds
-    var question7GuessCount = 0;
-    var q7UserCorrect = false;
-    var question7CorrectAnswers = ['Chiang Mai', 'Hanoi', 'Budapest', 'Munich', 'Playa Del Coco'];
+  var question7GuessCount = 0;
+  var q7UserCorrect = false;
+  var question7CorrectAnswers = ['Chiang Mai', 'Hanoi', 'Budapest', 'Munich', 'Playa Del Coco'];
 
-    while (question7GuessCount < 6 && q7UserCorrect === false) {
-        var answer7 = prompt('Can you guess one of my top favorite cities? Please answer with the city name only').toLowerCase();
-        console.log('The user answered ' + answer7 + ' to question 7.');
-        for (var i = 0; i < question7CorrectAnswers.length; i++) {
-            if (answer7 === question7CorrectAnswers[i].toLowerCase()) {
-                q7UserCorrect = true;
-                userAnswerTotal++;
+  while (question7GuessCount < 6 && q7UserCorrect === false) {
+    var answer7 = prompt('Can you guess one of my top favorite cities? Please answer with the city name only').toLowerCase();
+    console.log('The user answered ' + answer7 + ' to question 7.');
+    for (var i = 0; i < question7CorrectAnswers.length; i++) {
+      if (answer7 === question7CorrectAnswers[i].toLowerCase()) {
+        q7UserCorrect = true;
+        userAnswerTotal++;
                 //breaks out of the for loop if a match is found
-                break;
-            }
-        }
-        if (q7UserCorrect === false) {
-            alert('Sorry, you guessed wrong. Try again!');
-            question7GuessCount++;
-        }
+        break;
+      }
     }
-    if (q7UserCorrect === true) {
-        alert('You guessed right, congratulations! The correct answers were ' + question7CorrectAnswers + '.');
-    } else {
-        alert('Sorry, you\'re out of guesses. The correct answers were ' + question7CorrectAnswers + '.');
+    if (q7UserCorrect === false) {
+      alert('Sorry, you guessed wrong. Try again!');
+      question7GuessCount++;
     }
+  }
+  if (q7UserCorrect === true) {
+    alert('You guessed right, congratulations! The correct answers were ' + question7CorrectAnswers + '.');
+  } else {
+    alert('Sorry, you\'re out of guesses. The correct answers were ' + question7CorrectAnswers + '.');
+  }
 }
 
 Question1();
